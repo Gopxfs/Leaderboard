a6486b6a9413d33a7249a8ddb3bcb3da32cc2a25
Gabriel <gopxfs@hotmail.com>
2022-06-15 18:03:48 -0300

    Add add new score function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './scss/index.css';
+import API from './modules/API.js';
+
+const api = new API();
+const gameID = 'hM22QmJGo2GlE65TQ6ll';
+const submitScore = document.getElementById('submitScore');
+const refreshButton = document.getElementById('refreshButton');
+
+submitScore.addEventListener('submit', () => {
+  const playerName = document.getElementById('playerName');
+  const playerScore = document.getElementById('playerScore');
+  api.addNewScore(playerName.value, playerScore.value);
+});
+
+refreshButton.addEventListener('click', () => {
+  const gameScore = api.getGameScore();
+  console.log(gameScore);
+});
+
--- src/modules/API.js
+++ src/modules/API.js
@@ -1,7 +1,22 @@
 class API {
   
+  addNewScore(user, score) {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hM22QmJGo2GlE65TQ6ll/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "user": user,
+      "score": score,
+    }),
+    })
+  }
 
-  // https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ID/scores/
+  getGameScore() {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hM22QmJGo2GlE65TQ6ll/scores/')
+    .then(response => response.json())
+  }
 
   createGame(gameName) {
     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
