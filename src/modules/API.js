ae2a142361f727b6f78f7290bc9cff4be427a3d6
Gabriel <gopxfs@hotmail.com>
2022-06-15 22:30:15 -0300

    Add populate data function

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,34 +1,33 @@
 class API {
-  
-  addNewScore(user, score) {
-    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hM22QmJGo2GlE65TQ6ll/scores/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      "user": user,
-      "score": score,
-    }),
-    })
+  addNewScore = (user, score) => {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/EUPt0FcF5NjJxFAETzlU/scores/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+    });
   }
 
-   getGameScore() {
-    const score = fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hM22QmJGo2GlE65TQ6ll/scores/')
-    .then(response => response.json())
+  getGameScore = () => {
+    const score = fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/EUPt0FcF5NjJxFAETzlU/scores/')
+    .then((response) => response.json());
     return score;
   }
 
-  createGame(gameName) {
+  createGame = (gameName) => {
     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        "name": gameName,
+        name: gameName,
       }),
-    })
+    });
   }
 }
 
